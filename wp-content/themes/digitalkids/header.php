d8e427f6aa8d45ca268b8e5cc5a0ea57208304ec
scruchon <hello@stephane-cruchon.com>
2016-04-02 22:09:17 +0200

    Pour les pages on enlève la date featured

--- wp-content/themes/digitalkids/header.php
+++ wp-content/themes/digitalkids/header.php
@@ -12,11 +12,24 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<?php if(get_wanderer_option('favicon')) : ?>
-		<link rel="icon" href="<?php echo get_wanderer_option('favicon'); ?>" type="image/x-icon">
-	<?php else: ?>
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/x-icon">
-	<?php endif; ?>
+<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('template_directory'); ?>/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_directory'); ?>/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/favicon-16x16.png">
+<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
